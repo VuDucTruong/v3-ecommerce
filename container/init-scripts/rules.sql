4b79dfd1b6e85615668fe85dc85746eab9825920
Nguyen Pham Thanh Phong <thanhphong17160@gmail.com>
2025-05-08 11:49:58 +0700

    ..ooops

--- container/init-scripts/rules.sql
+++
@@ -1,8 +0,0 @@
-ALTER SYSTEM SET log_destination = 'stderr';
-ALTER SYSTEM SET logging_collector = on;
-ALTER SYSTEM SET log_directory = '/var/log/postgresql';
-ALTER SYSTEM SET log_filename = '%d_%m_%Y.log';
-ALTER SYSTEM SET log_rotation_age = '1d';
-ALTER SYSTEM SET log_rotation_size = '15MB';
-ALTER SYSTEM SET log_statement = 'all';
-ALTER SYSTEM SET log_line_prefix = '%t [%p]: db=%d,user=%u,app=%a';
