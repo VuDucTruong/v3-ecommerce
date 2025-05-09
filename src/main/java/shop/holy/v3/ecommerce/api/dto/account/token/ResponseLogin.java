03ae8d633b09e4d9d8572bdff78ca899af5a69af
Nguyen Pham Thanh Phong <thanhphong17160@gmail.com>
2025-05-09 21:47:44 +0700

    added email formats, changed comments response, changed response login

--- src/main/java/shop/holy/v3/ecommerce/api/dto/account/token/ResponseLogin.java
+++ src/main/java/shop/holy/v3/ecommerce/api/dto/account/token/ResponseLogin.java
@@ -1,6 +1,5 @@
 package shop.holy.v3.ecommerce.api.dto.account.token;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import shop.holy.v3.ecommerce.api.dto.account.ResponseUser;
 
 public record ResponseLogin (
