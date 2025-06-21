c4caa3cb7ffb44a8fa82b235036902b898cde3b8
Nguyen Pham Thanh Phong <thanhphong17160@gmail.com>
2025-06-21 16:15:11 +0700

    log bug trace metrics now done

--- src/main/java/shop/holy/v3/ecommerce/shared/config/OpenTelemetryConfig.java
+++ src/main/java/shop/holy/v3/ecommerce/shared/config/OpenTelemetryConfig.java
@@ -1,39 +1,40 @@
-//package shop.holy.v3.ecommerce.shared.config;
-//
-//import io.opentelemetry.api.OpenTelemetry;
-//import io.opentelemetry.api.common.Attributes;
-//import io.opentelemetry.context.propagation.ContextPropagators;
-//import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
-//import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
-//import io.opentelemetry.sdk.OpenTelemetrySdk;
-//import io.opentelemetry.sdk.logs.LogRecordProcessor;
-//import io.opentelemetry.sdk.logs.SdkLoggerProvider;
-//import io.opentelemetry.sdk.logs.SdkLoggerProviderBuilder;
-//import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
-//import io.opentelemetry.sdk.resources.Resource;
-//import io.opentelemetry.sdk.trace.SdkTracerProvider;
-//import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
-//import org.springframework.beans.factory.ObjectProvider;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.core.env.Environment;
-//
-//@Configuration
-//@Profile("ops")
-//public class OpenTelemetryConfig {
-//    @Bean
-//    OpenTelemetry openTelemetry(SdkLoggerProvider sdkLoggerProvider, SdkTracerProvider sdkTracerProvider, ContextPropagators contextPropagators) {
-//
-//        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
-//                .setLoggerProvider(sdkLoggerProvider)
-//                .setTracerProvider(sdkTracerProvider)
-//                .setPropagators(contextPropagators)
-//                .build();
-//        OpenTelemetryAppender.install(openTelemetrySdk);
-//        return openTelemetrySdk;
-//    }
-//
+package shop.holy.v3.ecommerce.shared.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.LogRecordProcessor;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.SdkLoggerProviderBuilder;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@Profile("ops")
+public class OpenTelemetryConfig {
+
+    @Bean
+    OpenTelemetry openTelemetry(SdkLoggerProvider sdkLoggerProvider, SdkTracerProvider sdkTracerProvider, ContextPropagators contextPropagators) {
+
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+                .setLoggerProvider(sdkLoggerProvider)
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(contextPropagators)
+                .build();
+        OpenTelemetryAppender.install(openTelemetrySdk);
+        return openTelemetrySdk;
+    }
+
 //    @Bean
 //    SdkLoggerProvider otelSdkLoggerProvider(Environment environment, ObjectProvider<LogRecordProcessor> logRecordProcessors) {
 //        String applicationName = environment.getProperty("spring.application.name", "application");
@@ -53,4 +54,4 @@
 //                                .build())
 //                .build();
 //    }
-//}
+}
